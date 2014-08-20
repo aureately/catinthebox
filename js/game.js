19fd04701f035f342ced8195c245e829ae7d1869
stanley zheng <dev.stanzheng@gmail.com>
2014-08-20 09:46:42 -0400

    adding initial example, it works

---
+++ js/game.js
@@ -0,0 +1,16 @@
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { create: create });
+
+function create() {
+  var text = "Cat In the Box";
+
+  var style = {
+    "font": "10em Helvetica",
+    "fill": "#f23232",
+    "margin-left":  "auto",
+    "margin-right": "auto"
+    };
+
+  var t = game.add.text(game.world.centerX-300, 0, text, style);
+
+}
